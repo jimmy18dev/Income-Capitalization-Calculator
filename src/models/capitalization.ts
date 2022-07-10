616fdb0e433a9f68f466daf535385227ac2ee090
Puwadon Sricharoen <mrjimmy18@gmail.com>
2022-07-10 11:29:38 +0700

    Move logics to capitalization class.

---
+++ src/models/capitalization.ts
@@ -0,0 +1,84 @@
+import numeral from 'numeral'
+
+interface ICapitalization {
+  rentPerMonth: number
+  expensesPerYear: number
+  capitalisationRate: number
+  installmentPerMonth: number
+  additionalPropertyValue: number
+  propertyPrice: number
+}
+
+export default class Capitalization {
+  public rentPerMonth: number
+  public expensesPerYear: number
+  public capitalisationRate: number
+  public installmentPerMonth: number
+  public additionalPropertyValue: number
+  public propertyPrice: number
+
+  constructor (data?: ICapitalization) {
+    this.rentPerMonth = data?.rentPerMonth || 0
+    this.expensesPerYear = data?.expensesPerYear || 0
+    this.capitalisationRate = data?.capitalisationRate || 0
+    this.installmentPerMonth = data?.installmentPerMonth || 0
+    this.additionalPropertyValue = data?.additionalPropertyValue || 0
+    this.propertyPrice = data?.propertyPrice || 0
+  }
+  
+  public propertyValue (): number {
+    const expensesPerYear = numeral(this.expensesPerYear).value() || 0
+    const capitalisationRate = numeral(this.capitalisationRate).value() || 0
+    const v = ((this.rentPerYear() - expensesPerYear) * 100) / capitalisationRate
+    return isFinite(v) ? Math.round(v) : 0
+  }
+
+  public cashOnCashRentalYield (): number {
+    const expensesPerYear = numeral(this.expensesPerYear).value() || 0
+    const additionalPropertyValue = numeral(this.additionalPropertyValue).value() || 0
+    const v = ((this.rentPerYear() - expensesPerYear - this.installmentPerYear ()) * 100) / (additionalPropertyValue || 1)
+    return isFinite(v) ? Number(v.toFixed(2)) : 0
+  }
+  
+  public cashflowPerMonth (): number {
+    const rentPerMonth = numeral(this.rentPerMonth).value() || 0
+    const installmentPerMonth = numeral(this.installmentPerMonth).value() || 0
+    const v = rentPerMonth - installmentPerMonth - this.expensesPerMonth()
+    return isFinite(v) ? Math.round(v) : 0
+  }
+
+  public cashflowPerYear (): number {
+    return this.perYear(this.cashflowPerMonth())
+  }
+
+  public netRentalYield (): number {
+    const expensesPerYear = numeral(this.expensesPerYear).value() || 0
+    const propertyPrice = numeral(this.propertyPrice).value() || 0
+    const additionalPropertyValue = numeral(this.additionalPropertyValue).value() || 0
+    const v = ((this.rentPerYear() - expensesPerYear) * 100) / (propertyPrice + additionalPropertyValue)
+    return isFinite(v) ? Number(v.toFixed(2)) : 0
+  }
+
+  public rentPerYear (): number {
+    const rentPerMonth = numeral(this.rentPerMonth).value() || 0
+    return this.perYear(rentPerMonth)
+  }
+
+  public installmentPerYear (): number {
+    const installmentPerMonth = numeral(this.installmentPerMonth).value() || 0
+    return this.perYear(installmentPerMonth)
+  }
+
+  public expensesPerMonth (): number {
+    const expensesPerYear = numeral(this.expensesPerYear).value() || 0
+    return this.perMonth(expensesPerYear)
+  }
+
+  private perYear (value: number): number {
+    return isFinite(value * 12) ? Math.round(value * 12) : 0
+  }
+
+  private perMonth (value: number): number {
+    return isFinite(value / 12) ? Math.round(value / 12) : 0
+  }
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,7 @@
     >
       <div class="flex flex-col mb-4">
         <InputPercentage
-          v-model:value="user.capitalisationRate"
+          v-model:value="capitalization.capitalisationRate"
           label="ผลตอบแทนที่ต้องการต่อปี"
           note="*ไม่ควรต่ำกว่าดอกเบี้ยสินเชื่อบ้าน-คอนโด (MRR)"
           placeholder="0.0"
@@ -19,7 +19,7 @@
       </div>
       <div class="flex flex-col mb-4">
         <InputPrice
-          v-model:value="user.expensesPerYear"
+          v-model:value="capitalization.expensesPerYear"
           label="ค่าส่วนกลางต่อปี (ถ้ามี)"
           placeholder="0.00"
           unit="บาท"
@@ -27,17 +27,17 @@
       </div>
       <div class="flex flex-col mb-4">
         <InputPrice
-          v-model:value="user.rentPerMonth"
+          v-model:value="capitalization.rentPerMonth"
           label="ค่าเช่าต่อเดือน"
           placeholder="0.00"
           unit="บาท"
-          :note="`≈ ${toBaht(user.rentPerYear)} บาทต่อปี`"
+          :note="`≈ ${toBaht(capitalization.rentPerYear())} บาทต่อปี`"
         />
       </div>
       <div class="flex items-baseline border-double border-b-4 border-gray-300 pb-1">
         <div class="flex-1">ราคาทรัพย์</div>
         <ResultPrice
-          :price="user.propertyValue"
+          :price="capitalization.propertyValue()"
         />
       </div>
     </Section>
@@ -48,16 +48,16 @@
     >
       <div class="flex flex-col mb-4">
         <InputPrice
-          v-model:value="user.installmentPerMonth"
+          v-model:value="capitalization.installmentPerMonth"
           label="ผ่อนธนาคารต่อเดือน"
           placeholder="0.00"
           unit="บาท"
-          :note="`≈ ${toBaht(user.installmentPerYear)} บาทต่อปี`"
+          :note="`≈ ${toBaht(capitalization.installmentPerYear())} บาทต่อปี`"
         />
       </div>
       <div class="flex flex-col mb-4">
         <InputPrice
-          v-model:value="user.additionalPropertyValue"
+          v-model:value="capitalization.additionalPropertyValue"
           label="ต้นทุนเพิ่มเติม"
           placeholder="0.00"
           note="เช่น เงินจอง, เงินดาวน์, ค่ารีโนเวท, ค่าเฟอร์นิเจอร์"
@@ -67,18 +67,18 @@
       <div class="flex pb-1">
         <div class="flex-1">ผลตอบแทน:</div>
         <ResultPercentage
-          :percentages="user.cashOnCashRentalYield"
+          :percentages="capitalization.cashOnCashRentalYield()"
         />
       </div>
       <div class="flex items-baseline border-double border-b-4 border-gray-300 pb-1">
         <div class="flex-1">กระแสเงินสด:</div>
         <div class="flex flex-col">
           <ResultCashflow
-            :cashflow="user.cashflowPerMonth"
+            :cashflow="capitalization.cashflowPerMonth()"
             prefix="ต่อเดือน"
           />
           <ResultCashflow
-            :cashflow="user.cashflowPerYear"
+            :cashflow="capitalization.cashflowPerYear()"
             prefix="ต่อปี"
           />
         </div>
@@ -92,7 +92,7 @@
     >
       <div class="flex flex-col mb-4">
         <InputPrice
-          v-model:value="user.propertyPrice"
+          v-model:value="capitalization.propertyPrice"
           label="ราคาทรัพย์"
           placeholder="0.00"
           unit="บาท"
@@ -101,7 +101,7 @@
       <div class="flex items-baseline border-double border-b-4 border-gray-300 pb-1">
         <div class="flex-1">ผลตอบแทน:</div>
         <ResultPercentage
-          :percentages="user.netRentalYield"
+          :percentages="capitalization.netRentalYield()"
         />
       </div>
     </Section>
@@ -118,8 +118,7 @@ import InputPercentage from '../components/InputPercentage.vue'
 import ResultPercentage from '../components/ResultPercentage.vue'
 import ResultCashflow from '../components/ResultCashflow.vue'
 import ResultPrice from '../components/ResultPrice.vue'
-import numeral from 'numeral'
-import User from '../interface/user'
+import Capitalization from '../models/capitalization'
 
 export default defineComponent({
   name: 'Home',
@@ -133,78 +132,21 @@ export default defineComponent({
     ResultPrice,
   },
   setup () {
-    const user: User = reactive({
+    const capitalization: Capitalization = reactive(new Capitalization({
       rentPerMonth: 6500,
-      rentPerYear: 0,
       expensesPerYear: 12000,
       capitalisationRate: 6.0,
-      propertyValue: 0,
       installmentPerMonth: 7000,
-      installmentPerYear: 0,
       additionalPropertyValue: 0,
       propertyPrice: 1200000,
-      netRentalYield: 0,
-      cashOnCashRentalYield: 0,
-      cashflowPerMonth: 0,
-      cashflowPerYear: 0,
-    })
+    }))
 
-    watch(() => user, () => {
-      const expensesPerYear = numeral(user.expensesPerYear).value() || 0
-      const rentPerMonth = numeral(user.rentPerMonth).value() || 0
-      const capitalisationRate = numeral(user.capitalisationRate).value() || 0
-      const additionalPropertyValue = numeral(user.additionalPropertyValue).value() || 0
-      const propertyPrice = numeral(user.propertyPrice).value() || 0
-      user.rentPerYear = perYear(rentPerMonth)
-      const installmentPerMonth = numeral(user.installmentPerMonth).value() || 0
-      user.installmentPerYear = perYear(installmentPerMonth)
-
-      user.propertyValue = calPropertyValue(user.rentPerYear, expensesPerYear, capitalisationRate)
-      user.netRentalYield = calNetRentalYield(user.rentPerYear, expensesPerYear, additionalPropertyValue, propertyPrice)
-      user.cashOnCashRentalYield = calCashOnCashRentalYield(user.rentPerYear, expensesPerYear, installmentPerMonth, additionalPropertyValue)
-      user.cashflowPerMonth = calCashflowPerMonth(rentPerMonth, expensesPerYear, installmentPerMonth)
-      user.cashflowPerYear = perYear(user.cashflowPerMonth)
+    watch(() => capitalization, () => {
+      console.log('capitalization watch.')
     }, { deep: true })
 
-    const perYear = (value: number): number => {
-      return isFinite(value * 12) ? Math.round(value * 12) : 0
-    }
-
-    const fitstCal = (): void => {
-      user.rentPerYear = perYear(user.rentPerMonth)
-      user.propertyValue = calPropertyValue(user.rentPerYear, user.expensesPerYear, user.capitalisationRate)
-      user.netRentalYield = calNetRentalYield(user.rentPerYear, user.expensesPerYear, user.additionalPropertyValue, user.propertyPrice)
-      user.cashOnCashRentalYield = calCashOnCashRentalYield(user.rentPerYear, user.expensesPerYear, user.installmentPerMonth, user.additionalPropertyValue)
-      user.cashflowPerMonth = calCashflowPerMonth(user.rentPerMonth, user.expensesPerYear, user.installmentPerMonth)
-      user.cashflowPerYear = perYear(user.cashflowPerMonth)
-    }
-
-    const calPropertyValue = (rentPerYear: number, expensesPerYear: number, capitalisationRate: number): number => {
-      const v = ((rentPerYear - expensesPerYear) * 100) / capitalisationRate
-      return isFinite(v) ? Math.round(v) : 0
-    }
-
-    const calNetRentalYield = (rentPerYear: number, expensesPerYear: number, additionalPropertyValue: number, propertyPrice: number): number => {
-      const v = ((rentPerYear - expensesPerYear) * 100) / (propertyPrice + additionalPropertyValue)
-      return isFinite(v) ? Number(v.toFixed(2)) : 0
-    }
-
-    const calCashOnCashRentalYield = (rentPerYear: number, expensesPerYear: number, installmentPerMonth: number, additionalPropertyValue: number): number => {
-      const v = ((rentPerYear - expensesPerYear - (installmentPerMonth * 12)) * 100) / (additionalPropertyValue || 1)
-      // console.log('v', v)
-      return isFinite(v) ? Number(v.toFixed(2)) : 0
-    }
-
-    const calCashflowPerMonth = (rentPerMonth: number, expensesPerYear: number, installmentPerMonth: number): number => {
-      const v = rentPerMonth - installmentPerMonth - (expensesPerYear / 12)
-      return isFinite(v) ? Math.round(v) : 0
-    }
-
-    onMounted(fitstCal)
-
     return {
-      user,
-      perYear,
+      capitalization,
     }
   },
   methods: {

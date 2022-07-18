87b40ea40aa9bcaa273da14c52b0cf7e212f36e4
Puwadon Sricharoen <mrjimmy18@gmail.com>
2022-07-18 19:35:24 +0700

    Reset vite.config to default.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig, loadEnv } from 'vite'
-import { createHtmlPlugin } from 'vite-plugin-html'
+import { defineConfig } from 'vite'
+// import { createHtmlPlugin } from 'vite-plugin-html'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
@@ -22,16 +22,5 @@ import vue from '@vitejs/plugin-vue'
 // }
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    createHtmlPlugin({
-      inject: {
-        data: {
-          title: 'คำนวณราคาทรัพย์กับการลงทุนเพื่อปล่อยเช่า',
-          description: 'ห้องที่กำลังจะตัดสินใจซื้อคุ้มค่ากับการลงทุนเพื่อปล่อยเช่าหรือไม่',
-          rootUrl: 'http://localhost:3000',
-        },
-      },
-    }),
-  ]
+  plugins: [vue()]
 })

becf11707b5e2927467eb363762a304aeec1cca5
Puwadon Sricharoen <mrjimmy18@gmail.com>
2022-07-18 19:41:19 +0700

    Code saved.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 // import { createHtmlPlugin } from 'vite-plugin-html'
 import vue from '@vitejs/plugin-vue'
 
@@ -21,6 +21,10 @@ import vue from '@vitejs/plugin-vue'
 //   })
 // }
 
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  console.log('env', env)
+  return {
+    plugins: [vue()]
+  }
 })

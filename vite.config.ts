b86f9305c52929db265fc6b24b6c5e368d69b62a
Puwadon Sricharoen <mrjimmy18@gmail.com>
2022-07-18 20:22:47 +0700

    Update typescript to 4.7.4

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import { createHtmlPlugin } from 'vite-plugin-html'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-// export default ({ mode }) => {
-//   // const env = loadEnv(mode, process.cwd())
-//   return defineConfig({
-//     plugins: [
-//       vue(),
-//       createHtmlPlugin({
-//         inject: {
-//           data: {
-//             title: 'คำนวณราคาทรัพย์กับการลงทุนเพื่อปล่อยเช่า',
-//             description: 'ห้องที่กำลังจะตัดสินใจซื้อคุ้มค่ากับการลงทุนเพื่อปล่อยเช่าหรือไม่',
-//             rootUrl: 'http://localhost:3000',
-//           },
-//         },
-//       }),
-//     ],
-//   })
-// }
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   console.log('env', env)

5b32bf2d4264d3833c447f1f0ec3b3719a5664d8
Puwadon Sricharoen <mrjimmy18@gmail.com>
2022-07-18 20:11:25 +0700

    Code saved.

--- vite.config.ts
+++ vite.config.ts
@@ -21,18 +21,21 @@ import vue from '@vitejs/plugin-vue'
 //   })
 // }
 
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   console.log('env', env)
   return {
     plugins: [
       vue(),
       createHtmlPlugin({
+        minify: true,
+        entry: 'src/main.ts',
+        template: './index.html',
         inject: {
           data: {
-            title: ''
-          }
-        }
+            title: 'index',
+          },
+        },
       })
     ]
   }

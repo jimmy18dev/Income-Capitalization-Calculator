e243d3c57c718064860243d76921bb6582cae35a
Puwadon Sricharoen <mrjimmy18@gmail.com>
2022-07-18 10:32:52 +0700

    Change vite config to defineConfig.

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,24 @@ import { createHtmlPlugin } from 'vite-plugin-html'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  // const env = loadEnv(mode, process.cwd())
-  return defineConfig({
-    plugins: [
-      vue(),
-      createHtmlPlugin({
-        inject: {
-          data: {
-            title: 'คำนวณราคาทรัพย์กับการลงทุนเพื่อปล่อยเช่า',
-            description: 'ห้องที่กำลังจะตัดสินใจซื้อคุ้มค่ากับการลงทุนเพื่อปล่อยเช่าหรือไม่',
-            rootUrl: 'http://localhost:3000',
-          },
-        },
-      }),
-    ],
-  })
-}
+// export default ({ mode }) => {
+//   // const env = loadEnv(mode, process.cwd())
+//   return defineConfig({
+//     plugins: [
+//       vue(),
+//       createHtmlPlugin({
+//         inject: {
+//           data: {
+//             title: 'คำนวณราคาทรัพย์กับการลงทุนเพื่อปล่อยเช่า',
+//             description: 'ห้องที่กำลังจะตัดสินใจซื้อคุ้มค่ากับการลงทุนเพื่อปล่อยเช่าหรือไม่',
+//             rootUrl: 'http://localhost:3000',
+//           },
+//         },
+//       }),
+//     ],
+//   })
+// }
+
+export default defineConfig({
+  plugins: [vue()]
+})

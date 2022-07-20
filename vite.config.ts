3ec290a9beb0968872789c5f38a0ce33460fc7d2
Puwadon Sricharoen <mrjimmy18@gmail.com>
2022-07-20 22:06:58 +0700

    Use createHtmlPlugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,33 @@
 import { defineConfig, loadEnv } from 'vite'
-import { createHtmlPlugin } from 'vite-plugin-html'
 import vue from '@vitejs/plugin-vue'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
-  const htmlPlugin = () => {
-    return {
-      name: "html-transform",
-      transformIndexHtml(html: string) {
-        return html.replace(/<%\s(.*?)\s%>/g, function (match, p1) {
-          return env[p1]
-        })
-      },
-    }
-  }
-  console.log('env', env)
+  // const htmlPlugin = () => {
+  //   return {
+  //     name: "html-transform",
+  //     transformIndexHtml(html: string) {
+  //       const r = html.replace(/<%\s(.*?)\s%>/g, (match, p1) => {
+  //         return env[p1]
+  //       })
+  //       console.log(r)
+  //       return r
+  //     },
+  //   }
+  // }
   return {
     plugins: [
       vue(),
-      htmlPlugin()
+      createHtmlPlugin({
+        // minify: true,
+        inject: {
+          data: {
+            title: 'ทดสอบ',
+            injectScript: `<script src="./inject.js"></script>`,
+          },
+        },
+      }),
     ]
   }
 })

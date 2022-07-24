fb0f10364bee656632cae6a91551be7dc64b5832
Puwadon Sricharoen <mrjimmy18@gmail.com>
2022-07-24 11:19:35 +0700

    Remove unused dependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,10 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VitePluginHtmlEnv from 'vite-plugin-html-env'
 
-export default defineConfig(({ mode }) => {
-  // const env = loadEnv(mode, process.cwd())
-  // const htmlPlugin = () => {
-  //   return {
-  //     name: "html-transform",
-  //     transformIndexHtml(html: string) {
-  //       // const r = html.replace(/<%\s(.*?)\s%>/g, (match, p1) => {
-  //       //   return env[p1]
-  //       // })
-  //       // console.log(r)
-  //       // return r
-  //       // const reg = new RegExp(`(${prefix}|<%)\\s+(\\w+)\\s+(${suffix}|\/>)`, 'g')
-  //       // return html.replace(reg, (...arg) => {
-  //       //   const key = arg[2]
-  //       //   return `${map[key]}`
-  //       // })
-  //     },
-  //   }
-  // }
-  return {
-    plugins: [
-      vue(),
-      VitePluginHtmlEnv(),
-    ]
-  }
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePluginHtmlEnv(),
+  ]
 })

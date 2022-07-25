ed3ef6c5d56ff6ad5f32664d680d5c8b86580b91
Puwadon Sricharoen <mrjimmy18@gmail.com>
2022-07-25 10:44:56 +0700

    Remove gif image.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="flex flex-col items-start p-5">
-    <div class="flex items-start">
-      <h1 class="text-2xl text-gray-900 flex-1 mr-3">{{ head }}</h1>
-      <img class="w-28" width="112" height="67" src="../assets/images/cover.jpg" alt="Income Capitalization Calculator" loading="lazy">
+    <div class="flex items-center">
+      <h1 class="text-2xl text-gray-900 flex-1 mr-5">{{ head }}</h1>
+      <div class="bg-purple-100 rounded-full w-20 h-20 flex justify-center items-center relative">
+        <font-awesome-icon class="text-4xl text-purple-600" :icon="['fas', 'building']" />
+        <font-awesome-icon class="absolute animate-bounce right-0 bottom-0 text-2xl text-yellow-500" :icon="['fas', 'sack-dollar']" />
+      </div>
     </div>
     <p class="text-gray-700 mt-3 text-lg">{{ description }}</p>
   </div>
